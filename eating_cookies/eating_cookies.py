ae09eec09ae18cf98df3cb26ddf3f8afdc0d4cbe
cladams0203 <cladams0203@gmail.com>
2020-07-07 14:22:04 -0400

    finished cookie monster MVP complete

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,25 @@
 Input: an integer
 Returns: an integer
 '''
+from functools import lru_cache
+
+
+
+
+
+@lru_cache(maxsize=5000)
+
 def eating_cookies(n):
     # Your code here
-
-    pass
+    cookiesAtOnce = [1, 2, 3]
+    if n == 0:
+        return 1
+    if n < 0:
+        return 0
+    count = 0
+    for i in range(len(cookiesAtOnce)):
+        count += eating_cookies(n - cookiesAtOnce[i])
+    return count
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
