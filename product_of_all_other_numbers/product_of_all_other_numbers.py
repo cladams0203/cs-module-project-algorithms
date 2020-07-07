b70bae72ae55923d0e9de759f1309027c3cee27c
cladams0203 <cladams0203@gmail.com>
2020-07-07 08:59:10 -0400

    finished product of all numbers

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -4,13 +4,33 @@ Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
     # Your code here
+    answer = []
+    pos = 0
+    while pos < len(arr):
+        temp = arr.pop(pos)
+        total = 0
+        for idx, i in enumerate(arr):
+            length = len(arr) - 1
+            if length == 0:
+                total = i
+            if idx < length:
+                if idx == 0:
+                    total = arr[idx] * arr[idx + 1]
+                else:
+                    total = total * arr[idx + 1]
+        answer.append(total)
+        total = 0
+        arr.insert(pos, temp)
+        pos += 1
 
-    pass
+    return answer
+
+
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
