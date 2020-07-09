27e5ed08e85a72c7bd604acfdd01877a26909c1e
cladams0203 <cladams0203@gmail.com>
2020-07-08 22:26:53 -0400

    finished optimizing moving zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,20 +2,41 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+'''
+First Day code 47 operations on python tutor
+'''
+# def moving_zeroes(arr):
+#     # Your code here
+#     zeros = []
+#     idx = 0
+#     length = len(arr) - 1
+#     while idx < length:
+#         if arr[idx] == 0:
+#             zeros.append(arr[idx])
+#             arr.pop(idx)
+#             length -= 1
+#         else:
+#             idx += 1
+#     for i in zeros:
+#         arr.append(i)
+#     return arr
+'''
+Second day code 39 operations on python tutor
+'''
 def moving_zeroes(arr):
-    # Your code here
-    zeros = []
-    idx = 0
-    length = len(arr) - 1
-    while idx < length:
-        if arr[idx] == 0:
-            zeros.append(arr[idx])
-            arr.pop(idx)
-            length -= 1
-        else:
-            idx += 1
-    for i in zeros:
-        arr.append(i)
+    left = 0
+    right = len(arr) - 1
+    while right > left:
+        if arr[left] == 0 and arr[right] != 0:
+            temp = arr[left]
+            arr[left] = arr[right]
+            arr[right] = temp
+            left += 1
+            right -= 1
+        if arr[left] != 0:
+            left += 1
+        if arr[right] == 0:
+            right -= 1
     return arr
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
